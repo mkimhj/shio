a4cd616350a553f30c3bd8ca9c6c23d30517cefa
Maruchi Kim <maruchi.kim@gmail.com>
2020-06-27 15:00:48 -0700

    Mic service from ITS transfer demo
    - Lot of code, not really following what it's doing but saving the work here for now

--- ble_manager.c
+++ ble_manager.c
@@ -74,14 +74,14 @@ void gatt_evt_handler(nrf_ble_gatt_t * p_gatt, nrf_ble_gatt_evt_t const * p_evt)
   if ((m_conn_handle == p_evt->conn_handle) && (p_evt->evt_id == NRF_BLE_GATT_EVT_ATT_MTU_UPDATED))
   {
     data_length = p_evt->params.att_mtu_effective - OPCODE_LENGTH - HANDLE_LENGTH;
-    //m_ble_params_info.mtu = m_ble_its_max_data_len;
+    //m_ble_params_info.mtu = m_ble_mic_service_max_data_len;
 
     NRF_LOG_INFO("gatt_event: ATT MTU is set to 0x%X (%d)", data_length, data_length);
   } else if ((m_conn_handle == p_evt->conn_handle) && (p_evt->evt_id == NRF_BLE_GATT_EVT_DATA_LENGTH_UPDATED)) {
     data_length = p_evt->params.att_mtu_effective - OPCODE_LENGTH - HANDLE_LENGTH - 4;
-    // m_ble_its_max_data_len = data_length;
-    // m_ble_params_info.mtu = m_ble_its_max_data_len;
-    // ble_its_ble_params_info_send(&m_its, &m_ble_params_info);
+    // m_ble_mic_service_max_data_len = data_length;
+    // m_ble_params_info.mtu = m_ble_mic_service_max_data_len;
+    // ble_mic_service_ble_params_info_send(&m_mic_service, &m_ble_params_info);
     NRF_LOG_INFO("gatt_event: Data len is set to 0x%X (%d)", data_length, data_length);
   }
   //  NRF_LOG_DEBUG("ATT MTU exchange completed. central 0x%x peripheral 0x%x",
@@ -113,36 +113,240 @@ static void nrf_qwr_error_handler(uint32_t nrf_error)
     APP_ERROR_HANDLER(nrf_error);
 }
 
-/**@brief Function for handling the YYY Service events.
- * YOUR_JOB implement a service handler function depending on the event the service you are using can generate
- *
- * @details This function will be called for all YY Service events which are passed to
- *          the application.
- *
- * @param[in]   p_yy_service   YY Service structure.
- * @param[in]   p_evt          Event received from the YY Service.
- *
- *
-static void on_yys_evt(ble_yy_service_t     * p_yy_service,
-                       ble_yy_service_evt_t * p_evt)
-{
-    switch (p_evt->evt_type)
-    {
-        case BLE_YY_NAME_EVT_WRITE:
-            APPL_LOG("[APPL]: charact written with value %s. ", p_evt->params.char_xx.value.p_str);
-            break;
 
-        default:
-            // No implementation needed.
-            break;
-    }
+#define BLE_UUID_MIC_TX_CHARACTERISTIC 0x0003                      /**< The UUID of the TX Characteristic. */
+#define BLE_UUID_MIC_RX_CHARACTERISTIC 0x0002                      /**< The UUID of the RX Characteristic. */
+#define BLE_MIC_MAX_RX_CHAR_LEN        BLE_MIC_MAX_DATA_LEN        /**< Maximum length of the RX Characteristic (in bytes). */
+#define BLE_MIC_MAX_TX_CHAR_LEN        BLE_MIC_MAX_DATA_LEN        /**< Maximum length of the TX Characteristic (in bytes). */
+
+
+typedef enum {
+  APP_CMD_NOCOMMAND = 0,
+  APP_CMD_SINGLE_CAPTURE,
+  APP_CMD_START_STREAM,
+  APP_CMD_STOP_STREAM,
+  APP_CMD_CHANGE_PHY,
+  APP_CMD_SEND_BLE_PARAMS
+} ble_mic_service_command_t;
+
+static bool m_stream_mode_active = false;
+static uint8_t m_new_command_received = 0;
+static uint8_t m_new_phy;
+
+static void mic_service_data_handler(ble_mic_service_t * p_mic_service, uint8_t const * p_data, uint16_t length)
+{
+  switch(p_data[0]) {
+    case APP_CMD_SINGLE_CAPTURE:
+    case APP_CMD_SEND_BLE_PARAMS:
+      m_new_command_received = p_data[0];
+      break;
+
+    case APP_CMD_START_STREAM:
+      m_stream_mode_active = true;
+      m_new_command_received = p_data[0];
+      break;
+
+    case APP_CMD_STOP_STREAM:
+      m_stream_mode_active = false;
+      m_new_command_received = p_data[0];
+      break;
+
+    case APP_CMD_CHANGE_PHY:
+      m_new_phy = p_data[1];
+      m_new_command_received = p_data[0];
+      break;
+
+    default:
+      NRF_LOG_ERROR("Unknown command!");
+      break;
+  }
 }
-*/
+
+void ble_mic_service_on_ble_evt(ble_evt_t const * p_ble_evt, void * p_context)
+{
+  if ((p_context == NULL) || (p_ble_evt == NULL)) {
+    return;
+  }
+
+  ble_mic_service_t * p_mic_service = (ble_mic_service_t*)p_context;
+
+  switch (p_ble_evt->header.evt_id) {
+    case BLE_GAP_EVT_CONNECTED:
+      NRF_LOG_INFO("BLE_GAP_EVT_CONNECTED");
+      // on_connect(p_mic_service, p_ble_evt);
+      break;
+
+    case BLE_GAP_EVT_DISCONNECTED:
+      NRF_LOG_INFO("BLE_GAP_EVT_DISCONNECTED");
+      // on_disconnect(p_mic_service, p_ble_evt);
+      break;
+
+    case BLE_GATTS_EVT_WRITE:
+      NRF_LOG_INFO("BLE_GATTS_EVT_WRITE");
+      // on_write(p_mic_service, p_ble_evt);
+      break;
+
+    case BLE_GATTS_EVT_HVN_TX_COMPLETE:
+    {
+      NRF_LOG_INFO("BLE_GATTS_EVT_HVN_TX_COMPLETE");
+      //uint32_t count = p_ble_evt->evt.gatts_evt.params.hvn_tx_complete.count;
+      // if(file_size > 0) {
+      //   push_data_packets();
+      // }
+      // nrf_error_resources = false;
+      break;
+    }
+
+
+    default:
+      // No implementation needed.
+      NRF_LOG_ERROR("ble_mic_service_on_ble_evt: not implemented, %d", p_ble_evt->header.evt_id);
+      break;
+  }
+}
+
+static uint32_t tx_char_add(ble_mic_service_t * p_mic_service, const ble_mic_service_init_t * p_mic_service_init)
+{
+  ble_gatts_char_md_t char_md;
+  ble_gatts_attr_md_t cccd_md;
+  ble_gatts_attr_t    attr_char_value;
+  ble_uuid_t          ble_uuid;
+  ble_gatts_attr_md_t attr_md;
+
+  memset(&cccd_md, 0, sizeof(cccd_md));
+
+  BLE_GAP_CONN_SEC_MODE_SET_OPEN(&cccd_md.read_perm);
+  BLE_GAP_CONN_SEC_MODE_SET_OPEN(&cccd_md.write_perm);
+
+  cccd_md.vloc = BLE_GATTS_VLOC_STACK;
+
+  memset(&char_md, 0, sizeof(char_md));
+
+  char_md.char_props.notify = 1;
+  char_md.p_char_user_desc  = NULL;
+  char_md.p_char_pf         = NULL;
+  char_md.p_user_desc_md    = NULL;
+  char_md.p_cccd_md         = &cccd_md;
+  char_md.p_sccd_md         = NULL;
+
+  ble_uuid.type = p_mic_service->uuid_type;
+  ble_uuid.uuid = BLE_UUID_MIC_TX_CHARACTERISTIC;
+
+  memset(&attr_md, 0, sizeof(attr_md));
+
+  BLE_GAP_CONN_SEC_MODE_SET_OPEN(&attr_md.read_perm);
+  BLE_GAP_CONN_SEC_MODE_SET_OPEN(&attr_md.write_perm);
+
+  attr_md.vloc    = BLE_GATTS_VLOC_STACK;
+  attr_md.rd_auth = 0;
+  attr_md.wr_auth = 0;
+  attr_md.vlen    = 1;
+
+  memset(&attr_char_value, 0, sizeof(attr_char_value));
+
+  attr_char_value.p_uuid    = &ble_uuid;
+  attr_char_value.p_attr_md = &attr_md;
+  attr_char_value.init_len  = sizeof(uint8_t);
+  attr_char_value.init_offs = 0;
+  attr_char_value.max_len   = BLE_MIC_MAX_TX_CHAR_LEN;
+
+  return sd_ble_gatts_characteristic_add(p_mic_service->service_handle,
+                                          &char_md,
+                                          &attr_char_value,
+                                          &p_mic_service->tx_handles);
+}
+
+static uint32_t rx_char_add(ble_mic_service_t * p_mic_service, const ble_mic_service_init_t * p_mic_service_init)
+{
+  ble_gatts_char_md_t char_md;
+  ble_gatts_attr_t    attr_char_value;
+  ble_uuid_t          ble_uuid;
+  ble_gatts_attr_md_t attr_md;
+
+  memset(&char_md, 0, sizeof(char_md));
+
+  char_md.char_props.write         = 1;
+  char_md.char_props.write_wo_resp = 1;
+  char_md.p_char_user_desc         = NULL;
+  char_md.p_char_pf                = NULL;
+  char_md.p_user_desc_md           = NULL;
+  char_md.p_cccd_md                = NULL;
+  char_md.p_sccd_md                = NULL;
+
+  ble_uuid.type = p_mic_service->uuid_type;
+  ble_uuid.uuid = BLE_UUID_MIC_RX_CHARACTERISTIC;
+
+  memset(&attr_md, 0, sizeof(attr_md));
+
+  BLE_GAP_CONN_SEC_MODE_SET_OPEN(&attr_md.read_perm);
+  BLE_GAP_CONN_SEC_MODE_SET_OPEN(&attr_md.write_perm);
+
+  attr_md.vloc    = BLE_GATTS_VLOC_STACK;
+  attr_md.rd_auth = 0;
+  attr_md.wr_auth = 0;
+  attr_md.vlen    = 1;
+
+  memset(&attr_char_value, 0, sizeof(attr_char_value));
+
+  attr_char_value.p_uuid    = &ble_uuid;
+  attr_char_value.p_attr_md = &attr_md;
+  attr_char_value.init_len  = 1;
+  attr_char_value.init_offs = 0;
+  attr_char_value.max_len   = BLE_MIC_MAX_RX_CHAR_LEN;
+
+  return sd_ble_gatts_characteristic_add(p_mic_service->service_handle,
+                                          &char_md,
+                                          &attr_char_value,
+                                          &p_mic_service->rx_handles);
+}
+
+/**< Used vendor specific UUID. */
+#define MIC_SERVICE_BASE_UUID {{0x3E, 0xCA, 0xDC, 0x24, 0x0E, 0xE5, 0xA9, 0xE0, 0x93, 0xF3, 0xA3, 0xB5, 0x00, 0x00, 0x40, 0x6E}}
+#define BLE_UUID_MIC_SERVICE 0x0001                      /**< The UUID of the Nordic UART Service. */
+
+uint32_t ble_mic_service_init(ble_mic_service_t * p_mic_service, const ble_mic_service_init_t * p_mic_service_init)
+{
+  uint32_t      err_code;
+  ble_uuid_t    ble_uuid;
+  ble_uuid128_t mic_service_base_uuid = MIC_SERVICE_BASE_UUID;
+
+  VERIFY_PARAM_NOT_NULL(p_mic_service);
+  VERIFY_PARAM_NOT_NULL(p_mic_service_init);
+
+  // Initialize the service structure.
+  p_mic_service->conn_handle             = BLE_CONN_HANDLE_INVALID;
+  p_mic_service->data_handler            = p_mic_service_init->data_handler;
+  p_mic_service->is_notification_enabled = false;
+
+  // Add a custom base UUID.
+  err_code = sd_ble_uuid_vs_add(&mic_service_base_uuid, &p_mic_service->uuid_type);
+  VERIFY_SUCCESS(err_code);
+
+  ble_uuid.type = p_mic_service->uuid_type;
+  ble_uuid.uuid = BLE_UUID_MIC_SERVICE;
+
+  // Add the service.
+  err_code = sd_ble_gatts_service_add(BLE_GATTS_SRVC_TYPE_PRIMARY,
+                                      &ble_uuid,
+                                      &p_mic_service->service_handle);
+  VERIFY_SUCCESS(err_code);
+
+  // Add the RX Characteristic.
+  err_code = rx_char_add(p_mic_service, p_mic_service_init);
+  VERIFY_SUCCESS(err_code);
+
+  // Add the TX Characteristic.
+  err_code = tx_char_add(p_mic_service, p_mic_service_init);
+  VERIFY_SUCCESS(err_code);
+
+  return NRF_SUCCESS;
+}
 
 void services_init(void)
 {
-  ret_code_t         err_code;
-  nrf_ble_qwr_init_t qwr_init = {0};
+  ret_code_t             err_code;
+  nrf_ble_qwr_init_t     qwr_init = {0};
+  ble_mic_service_init_t mic_service_init;
 
   // Initialize Queued Write Module.
   qwr_init.error_handler = nrf_qwr_error_handler;
@@ -150,7 +354,13 @@ void services_init(void)
   err_code = nrf_ble_qwr_init(&m_qwr, &qwr_init);
   APP_ERROR_CHECK(err_code);
 
-  // Initialize mic stream service here
+  // Initialize mic service
+  memset(&mic_service_init, 0, sizeof(mic_service_init));
+
+  mic_service_init.data_handler = mic_service_data_handler;
+
+  err_code = ble_mic_service_init(&m_mic_service, &mic_service_init);
+  APP_ERROR_CHECK(err_code);
 }
 
 static void on_conn_params_evt(ble_conn_params_evt_t * p_evt)
@@ -246,7 +456,7 @@ static void ble_evt_handler(ble_evt_t const * p_ble_evt, void * p_context)
             m_conn_handle = p_ble_evt->evt.gap_evt.conn_handle;
             err_code = nrf_ble_qwr_conn_handle_assign(&m_qwr, m_conn_handle);
             APP_ERROR_CHECK(err_code);
-            // ble_its_ble_params_info_send(&m_its, &m_ble_params_info);
+            // ble_mic_service_ble_params_info_send(&m_mic_service, &m_ble_params_info);
             break;
 
         case BLE_GAP_EVT_CONN_PARAM_UPDATE:
@@ -254,11 +464,10 @@ static void ble_evt_handler(ble_evt_t const * p_ble_evt, void * p_context)
           uint16_t max_con_int = p_ble_evt->evt.gap_evt.params.conn_param_update.conn_params.max_conn_interval;
           uint16_t min_con_int = p_ble_evt->evt.gap_evt.params.conn_param_update.conn_params.min_conn_interval;
           // m_ble_params_info.con_interval = max_con_int;
-          // ble_its_ble_params_info_send(&m_its, &m_ble_params_info);
+          // ble_mic_service_ble_params_info_send(&m_mic_service, &m_ble_params_info);
           NRF_LOG_INFO("Con params updated: CI %i, %i", (int)min_con_int, (int)max_con_int);
           break;
         }
-
 
         case BLE_GAP_EVT_PHY_UPDATE_REQUEST:
         {
@@ -277,7 +486,7 @@ static void ble_evt_handler(ble_evt_t const * p_ble_evt, void * p_context)
         {
           int tx_phy = p_ble_evt->evt.gap_evt.params.phy_update.tx_phy;
           int rx_phy = p_ble_evt->evt.gap_evt.params.phy_update.rx_phy;
-          // ble_its_ble_params_info_send(&m_its, &m_ble_params_info);
+          // ble_mic_service_ble_params_info_send(&m_mic_service, &m_ble_params_info);
           NRF_LOG_INFO("Phy update: %i, %i", tx_phy, rx_phy);
           break;
         }
--- ble_manager.h
+++ ble_manager.h
@@ -2,16 +2,48 @@
 #define HANDLE_LENGTH 2
 
 #if defined(NRF_SDH_BLE_GATT_MAX_MTU_SIZE) && (NRF_SDH_BLE_GATT_MAX_MTU_SIZE != 0)
-    #define BLE_ITS_MAX_DATA_LEN (NRF_SDH_BLE_GATT_MAX_MTU_SIZE - OPCODE_LENGTH - HANDLE_LENGTH) /**< Maximum length of data (in bytes) that can be transmitted to the peer by the Nordic UART service module. */
+    #define BLE_MIC_MAX_DATA_LEN (NRF_SDH_BLE_GATT_MAX_MTU_SIZE - OPCODE_LENGTH - HANDLE_LENGTH) /**< Maximum length of data (in bytes) that can be transmitted to the peer by the Nordic UART service module. */
 #else
-    #define BLE_ITS_MAX_DATA_LEN (NRF_SDH_BLE_GATT_MAX_MTU_SIZE_DEFAULT - OPCODE_LENGTH - HANDLE_LENGTH) /**< Maximum length of data (in bytes) that can be transmitted to the peer by the Nordic UART service module. */
+    #define BLE_MIC_MAX_DATA_LEN (NRF_SDH_BLE_GATT_MAX_MTU_SIZE_DEFAULT - OPCODE_LENGTH - HANDLE_LENGTH) /**< Maximum length of data (in bytes) that can be transmitted to the peer by the Nordic UART service module. */
     #warning NRF_BLE_GATT_MAX_MTU_SIZE is not defined.
 #endif
+
+// START OF CUSTOM MIC BLE SERVICE DEFINITIONS
+void ble_mic_service_on_ble_evt(ble_evt_t const * p_ble_evt, void * p_context);
+
+#define BLE_MIC_SERVICE_DEF(_name)               \
+static ble_mic_service_t _name;                  \
+NRF_SDH_BLE_OBSERVER(_name ## _obs,              \
+                     BLE_NUS_BLE_OBSERVER_PRIO,  \
+                     ble_mic_service_on_ble_evt, \
+                     &_name);
+
+typedef struct ble_mic_service_s ble_mic_service_t;
+
+/**@brief Nordic UART Service event handler type. */
+typedef void (*ble_mic_service_data_handler_t) (ble_mic_service_t * p_mic_service, uint8_t const * p_data, uint16_t length);
+
+typedef struct ble_mic_service_s {
+  uint8_t                  uuid_type;               /**< UUID type for Nordic UART Service Base UUID. */
+  uint16_t                 service_handle;          /**< Handle of Nordic UART Service (as provided by the SoftDevice). */
+  ble_gatts_char_handles_t tx_handles;              /**< Handles related to the TX characteristic (as provided by the SoftDevice). */
+  ble_gatts_char_handles_t rx_handles;              /**< Handles related to the RX characteristic (as provided by the SoftDevice). */
+  uint16_t                 conn_handle;             /**< Handle of the current connection (as provided by the SoftDevice). BLE_CONN_HANDLE_INVALID if not in a connection. */
+  bool                     is_notification_enabled; /**< Variable to indicate if the peer has enabled notification of the RX characteristic.*/
+  bool                     is_info_char_notification_enabled;
+  ble_mic_service_data_handler_t   data_handler;            /**< Event handler to be called for handling received data. */
+} ble_mic_service_t;
+
+typedef struct {
+  ble_mic_service_data_handler_t data_handler; /**< Event handler to be called for handling received data. */
+} ble_mic_service_init_t;
+
+BLE_MIC_SERVICE_DEF(m_mic_service);
+// END OF CUSTOM MIC BLE SERVICE DEFINITIONS
 
 NRF_BLE_GATT_DEF(m_gatt);                                                       /**< GATT module instance. */
 NRF_BLE_QWR_DEF(m_qwr);                                                         /**< Context for the Queued Write module.*/
 BLE_ADVERTISING_DEF(m_advertising);                                             /**< Advertising module instance. */
-BLE_MIC_SERVICE_DEF(m_mic_service);
 
 static uint16_t m_conn_handle = BLE_CONN_HANDLE_INVALID;                        /**< Handle of the current connection. */
 
