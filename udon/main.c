95986b8327a31cf7411f068002c0056b998e33b4
wanganran <wanganran01@gmail.com>
2020-09-20 00:23:58 -0700

    PWM output at rx side

--- udon/main.c
+++ udon/main.c
@@ -49,6 +49,60 @@
 #include "nrf_timer.h"
 #include "nrf_gpiote.h"
 
+/**** PWM related ****/
+#define PWM_PIN NRF_GPIO_PIN_MAP(1, 7)
+#include "nrf_drv_pwm.h"
+#include "nrf_drv_clock.h"
+static nrf_drv_pwm_t m_pwm0 = NRF_DRV_PWM_INSTANCE(0);
+nrf_pwm_values_individual_t seq_values[] = {{10, 0, 0, 0}};
+nrf_pwm_sequence_t const seq =
+{
+    .values.p_individual = seq_values,
+    .length          = NRF_PWM_VALUES_LENGTH(seq_values),
+    .repeats         = 0,
+    .end_delay       = 0
+};
+
+void pwm_init(){
+  nrf_drv_clock_lfclk_request(NULL);
+
+  // init PWM for 50kHz clock
+  // Start clock for accurate frequencies
+  NRF_CLOCK->TASKS_HFCLKSTART = 1; 
+  // Wait for clock to start
+  while(NRF_CLOCK->EVENTS_HFCLKSTARTED == 0);
+  nrf_drv_pwm_config_t const config0 =
+  {
+      .output_pins =
+      {
+          PWM_PIN, // channel 0
+          NRF_DRV_PWM_PIN_NOT_USED,             // channel 1
+          NRF_DRV_PWM_PIN_NOT_USED,             // channel 2
+          NRF_DRV_PWM_PIN_NOT_USED,             // channel 3
+      },
+      .irq_priority = APP_IRQ_PRIORITY_LOWEST,
+      .base_clock   = NRF_PWM_CLK_1MHz,
+      .count_mode   = NRF_PWM_MODE_UP,
+      .top_value    = 20,
+      .load_mode    = NRF_PWM_LOAD_INDIVIDUAL,
+      .step_mode    = NRF_PWM_STEP_AUTO
+  };
+
+  // Init PWM without error handler
+  APP_ERROR_CHECK(nrf_drv_pwm_init(&m_pwm0, &config0, NULL));
+
+  nrf_drv_pwm_simple_playback(&m_pwm0, &seq, 1, NRF_DRV_PWM_FLAG_LOOP);
+  // end
+}
+
+
+/**** end PWM related ****/
+
+#define START_PIN NRF_GPIO_PIN_MAP(1, 6)
+#define EN_IND_PIN 3
+#define TS_IND_PIN 2
+
+
 #define LED_BLE_NUS_CONN (BSP_BOARD_LED_0)
 #define LED_BLE_NUS_RX   (BSP_BOARD_LED_1)
 #define LED_CDC_ACM_CONN (BSP_BOARD_LED_2)
@@ -182,6 +236,7 @@ void gatt_init(void)
 void bsp_event_handler(bsp_event_t event)
 {
     uint32_t err_code;
+    static bool m_start_clock=false;
     switch (event)
     {
         case BSP_EVENT_SLEEP:
@@ -207,6 +262,29 @@ void bsp_event_handler(bsp_event_t event)
             }
             break;
 
+        case BSP_EVENT_KEY_3:
+
+            if (m_start_clock)
+            {
+                m_start_clock = false;
+
+                bsp_board_led_off(EN_IND_PIN);
+
+                nrf_gpio_pin_clear(START_PIN);
+
+                NRF_LOG_INFO("en stop\n");
+            }
+            else
+            {
+                m_start_clock = true;
+
+                bsp_board_led_on(EN_IND_PIN);
+
+                nrf_gpio_pin_set(START_PIN);
+
+                NRF_LOG_INFO("en start\n");
+            }
+            break;
         default:
             break;
     }
@@ -485,6 +563,7 @@ int main(void)
     app_usbd_serial_num_generate();
 
     ret = nrf_drv_clock_init();
+    pwm_init();
     APP_ERROR_CHECK(ret);
 
     NRF_LOG_INFO("udon booted");
